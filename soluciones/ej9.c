c0d604de967d94f8fbc4e856c5a5a245161c572b
GonzalezPerezLuisAngel <luisangelgonzaxl@gmail.com>
2026-02-24 14:48:20 -0600

    Ejercicio9.c

--- soluciones/ej9.c
+++ soluciones/ej9.c
@@ -1,6 +1,11 @@
 
 #include <stdio.h>
-int main(){
-    // Resolver ejercicio 9
+int main()
+{
+    int a[5];
+
+    printf("&a[-1] = %p\n", &a[-1]);   //Imprime una direccion de memoria antes del arreglo
+    printf("&a[5]  = %p\n", &a[5]);    //Imprime una direccion de memoria despues del arreglo
+
     return 0;
 }
