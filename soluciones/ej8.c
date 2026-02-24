38c1ceba22f1f2bfea8b2fae764ede7c81eb99ab
GonzalezPerezLuisAngel <luisangelgonzaxl@gmail.com>
2026-02-24 14:45:31 -0600

    Ejercicio8.c

--- soluciones/ej8.c
+++ soluciones/ej8.c
@@ -1,6 +1,10 @@
 
 #include <stdio.h>
 int main(){
-    // Resolver ejercicio 8
+    int m[2][4];
+
+    printf("&m[0][0] = %p\n", &m[0][0]);
+    printf("&m[0][0] = %p\n", &m[1][2]);
+    
     return 0;
 }
